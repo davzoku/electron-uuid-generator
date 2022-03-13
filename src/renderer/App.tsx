9d51c285f98a3666145dd3881d5ef554ec3e74b5
Kok Wai <teng.kokwai93@gmail.com>
2022-03-14 00:41:11 +0800

    impl simple uuid v4 gen

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,39 +1,17 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 const Hello = () => {
+  const [uuid, setUuid] = useState(uuidv4());
+
   return (
     <div>
       <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+        <h1>Your version 4 UUID:</h1>
+        <p>{uuid}</p>
+        <button onClick={() => setUuid(uuidv4())}>Generate</button>
       </div>
     </div>
   );
